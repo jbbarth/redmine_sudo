6f1aaa74e486bf87de26dbad078cf927934eb4d9
jbbarth <jeanbaptiste.barth@gmail.com>
2012-06-10 21:56:43 +0200

    Renamed action to UserController#sudo_toggle + renamed routes so that it's not ambiguous in Rails 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 if Redmine::VERSION::MAJOR <= 1
   # Rails 2.3
   ActionController::Routing::Routes.draw do |map|
-    map.connect 'users/sudo', :controller => 'users', :action => 'sudo'
+    map.connect 'users/sudo/toggle', :controller => 'users', :action => 'sudo'
   end
 else
   # Rails 3
   RedmineApp::Application.routes.draw do
-    match 'users/sudo', :to => 'users#sudo'
+    match 'users/sudo/toggle', :to => 'users#sudo_toggle'
   end
 end
--- lib/redmine_sudo/users_controller_patch.rb
+++ lib/redmine_sudo/users_controller_patch.rb
@@ -3,7 +3,7 @@ require_dependency 'users_controller'
 class UsersController
   before_filter :require_admin, :except => [:show, :sudo]
 
-  def sudo
+  def sudo_toggle
     require_login #should render a 403 if no user found
     render_403 unless User.current.sudoer?
     User.current.update_admin!( !User.current.admin? )
