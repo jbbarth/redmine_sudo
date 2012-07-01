5fffb7729dda64586bf65ccbd5daea7e80834e2a
jbbarth <jeanbaptiste.barth@gmail.com>
2012-07-02 00:04:03 +0200

    Make test/test_helper.rb compatible with the new plugins directory

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
-# Load the normal Rails helper
-require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
+require 'redmine'
 
-# Ensure that we are using the temporary fixture path
-Engines::Testing.set_fixture_path
+if Redmine::VERSION::MAJOR <= 1
+  # Rails 2.3
+  require File.expand_path(File.dirname(__FILE__) + '/../../../../test/test_helper')
+  Engines::Testing.set_fixture_path
+else
+  # Rails 3
+  require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
+end
