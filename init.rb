2831354de7094201e568ec1226de57aef22f8a40
jbbarth <jeanbaptiste.barth@gmail.com>
2012-09-26 13:49:19 +0200

    Consistency with my other plugins (2)

--- init.rb
+++ init.rb
@@ -15,7 +15,7 @@ Redmine::Plugin.register :redmine_sudo do
   description 'This plugin gives sudo-like powers to Redmine administrators'
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  url 'http://github.com/jbbarth/redmine_sudo'
+  url 'https://github.com/jbbarth/redmine_sudo'
   version '0.0.1'
   requires_redmine :version_or_higher => '2.1.0'
   settings :default => {
