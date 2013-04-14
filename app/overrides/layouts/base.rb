3a9fb318f5f8ba5c5c336341aa3446d5b5382699
jbbarth <jeanbaptiste.barth@gmail.com>
2013-04-15 01:12:08 +0200

    Add :original keys to deface overrides

--- app/overrides/layouts/base.rb
+++ app/overrides/layouts/base.rb
@@ -1,4 +1,5 @@
 Deface::Override.new :virtual_path  => 'layouts/base',
                      :name          => 'add-sudo-toggle-to-layout',
+                     :original      => 'a86c746507aa0564288f2ad0cf970db6c348a57a',
                      :insert_after  => '#account',
                      :partial       => 'sudo/sudo_link'
