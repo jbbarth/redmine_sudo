aeca2e98d0ffb18d2c0e93641b7ace3ac8cc20cd
Vincent Robert <vincent.robert@nanego.com>
2015-04-27 15:43:56 +0200

    Specify http methods when using match to define new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 RedmineApp::Application.routes.draw do
-  match 'sudo/toggle', :to => 'sudo#toggle', :as => 'sudo_toggle'
+  match 'sudo/toggle', :to => 'sudo#toggle', :as => 'sudo_toggle', via: [:get, :post]
 end
