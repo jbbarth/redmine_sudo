f82b3c3825d393e293bb57c8bb8020e093748849
a.fedorova <a.fedorova@elins.ru>
2017-04-24 15:43:15 +0300

    syntax compatibility for older versions

--- db/migrate/002_give_sudoer_rights_to_admins.rb
+++ db/migrate/002_give_sudoer_rights_to_admins.rb
@@ -1,11 +1,11 @@
 class GiveSudoerRightsToAdmins < ActiveRecord::Migration
 
   def self.up
-    User.where(admin: true).update_all(sudoer: true)
+    User.where(:admin => true).update_all(:sudoer => true)
   end
 
   def self.down
-    User.where(sudoer: true).update_all(admin: true, sudoer: false)
+    User.where(:sudoer => true).update_all(:admin => true, :sudoer => false)
   end
 
 end
