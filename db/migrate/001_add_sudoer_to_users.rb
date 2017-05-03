bad3008bf6979e382fc24e8654472988e07f1eec
a.fedorova <a.fedorova@elins.ru>
2017-05-03 14:54:44 +0300

    Исправления по review 4611

--- db/migrate/001_add_sudoer_to_users.rb
+++ db/migrate/001_add_sudoer_to_users.rb
@@ -1,9 +1,9 @@
 class AddSudoerToUsers < ActiveRecord::Migration
-  def self.up
+  def up
     add_column :users, :sudoer, :boolean, :default => false, :null => false
   end
 
-  def self.down
+  def down
     remove_column :users, :sudoer
   end
 end
--- db/migrate/002_give_sudoer_rights_to_admins.rb
+++ db/migrate/002_give_sudoer_rights_to_admins.rb
@@ -1,11 +1,9 @@
 class GiveSudoerRightsToAdmins < ActiveRecord::Migration
-
-  def self.up
+  def up
     User.where(:admin => true).update_all(:sudoer => true)
   end
 
-  def self.down
+  def down
     User.where(:sudoer => true).update_all(:admin => true, :sudoer => false)
   end
-
 end
