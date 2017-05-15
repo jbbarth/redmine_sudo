19803f5a0540936a6018b1fb891bc2308349e779
a.fedorova <a.fedorova@elins.ru>
2017-05-15 16:37:17 +0300

    feature test refactoring

--- spec/routing/routing_spec.rb
+++ spec/routing/routing_spec.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../../rails_helper', __FILE__)
 
-describe 'Sudo toggle path' do
+describe 'Sudo toggle link' do
   it 'should route to sudo#toggle' do
     expect(get: '/sudo/toggle').to route_to(controller: 'sudo', action: 'toggle')
   end
