f91ee8c8e3be803037fe077365c86423482924e9
Yalaeddin <89978016+Yalaeddin@users.noreply.github.com>
2023-08-02 11:52:05 +0200

    Fix missing link on mobile devices

--- app/controllers/sudo_controller.rb
+++ app/controllers/sudo_controller.rb
@@ -3,6 +3,8 @@ class SudoController < ApplicationController
     require_login #should render a 403 if no user found
     if User.current.sudoer?
       User.current.update_admin!( !User.current.admin? )
+
+      params[:back_url] = url_for(request.referer) if request.referer.present?
       redirect_back_or_default :controller => "my", :action => "page"
     else
       render_403
