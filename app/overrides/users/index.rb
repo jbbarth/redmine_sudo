29e80ce3129b0b510d683982f945114a1c89448b
Vincent Robert <vincent.robert@nanego.com>
2023-08-03 13:33:40 +0200

    Do not override all 'ticks' on users list

--- app/overrides/users/index.rb
+++ app/overrides/users/index.rb
@@ -1,5 +1,5 @@
 Deface::Override.new :virtual_path  => 'users/index',
                      :name          => 'add-sudoer-info-to-admin-users-table',
                      :original      => 'edc31bce1cac515de516f73c53f92a61c942301f',
-                     :replace       => 'td.tick',
+                     :replace       => 'td.tick:first',
                      :partial       => 'sudo/users-table'
