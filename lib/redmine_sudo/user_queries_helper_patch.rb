acf09486a0ba8d00ba4ac15463fde0de1153dc6f
Vincent <contact@vincent-robert.com>
2026-02-25 17:16:40 +0100

    Avoid broken super chain in UserQueriesHelperPatch

--- lib/redmine_sudo/user_queries_helper_patch.rb
+++ lib/redmine_sudo/user_queries_helper_patch.rb
@@ -7,7 +7,7 @@ module RedmineSudo
     # Show admin column as "Yes" for sudoers even when they're not currently admin
     def column_value(column, object, value)
       if object.is_a?(User) && column.name == :admin
-        super(column, object, object.admin? || object.sudoer?)
+        l(object.admin? || object.sudoer? ? :general_text_Yes : :general_text_No)
       else
         super
       end
