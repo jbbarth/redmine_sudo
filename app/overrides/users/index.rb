f936f3d37fe780e66e0da8f36307e64a9f42cf58
Vincent <contact@vincent-robert.com>
2026-07-23 09:10:42 +0200

    Add default css for Redmine 7.0

--- app/overrides/users/index.rb
+++ app/overrides/users/index.rb
@@ -1,5 +1,4 @@
 Deface::Override.new :virtual_path  => 'users/index',
                      :name          => 'add-sudoer-info-to-admin-users-table',
-                     :original      => 'edc31bce1cac515de516f73c53f92a61c942301f',
                      :replace       => 'td.tick:first',
                      :partial       => 'sudo/users-table'
